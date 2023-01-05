6484135ba9dbef7e8dd700917cc690fc37d4004e
hzp <hzhouping@outlook.com>
2023-01-05 11:48:28 +0800

    feat: 解决 ts 报错

--- types/env.d.ts
+++ types/env.d.ts
@@ -7,3 +7,9 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+declare module '*.vue' {
+  import { defineComponent } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
+}
