b2dfc6d3a98ccb3b4e70e2e43b2e50f4ecb37b5b
hzp <hzhouping@outlook.com>
2023-01-06 13:59:45 +0800

    feat: 配置 vite 开发服务器

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,10 +2,10 @@ export interface ImportMetaEnv {
   readonly APP_NAME: string
 
   readonly VITE_PORT: number
+  readonly VITE_PROXY: Array<ProxyList>
   readonly VITE_LEGACY: boolean
   readonly VITE_BUILD_COMPRESSION: string
   readonly VITE_COMPRESSION_DELETE_ORIGIN_FILE: boolean
-  readonly VITE_PROXY: Array<ProxyList>
 }
 
 interface ImportMeta {
